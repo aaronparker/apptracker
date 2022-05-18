ab06de3fb2d175e348818b212f9552bce4957424
Aaron Parker <aaron@stealthpuppy.com>
2022-05-18 20:28:06 +1000

    Update For loop

--- scripts/Update-Json.ps1
+++ scripts/Update-Json.ps1
@@ -1,6 +1,7 @@
 <#
     Queries each application in Evergreen and exports the result to JSON
 #>
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "")]
 [CmdletBinding(SupportsShouldProcess = $true)]
 param(
     [ValidateNotNullOrEmpty()]
@@ -33,13 +34,13 @@ Function Test-PSCore {
 
 # Step through all apps and export result to JSON
 if (Test-PSCore) {
-    Find-EvergreenApp | Select-Object -ExpandProperty "Name" | `
-        ForEach-Object {
-        $Output = Get-EvergreenApp -Name $_ -ErrorAction "SilentlyContinue" -WarningAction "SilentlyContinue"
+    foreach ($App in (Find-EvergreenApp | Select-Object -ExpandProperty "Name")) {
+
+        $Output = Get-EvergreenApp -Name $App -ErrorAction "SilentlyContinue" -WarningAction "SilentlyContinue"
         if (($Null -ne $Output) -or ($Output.Version -ne "RateLimited")) {
+
             $Output | Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true }, "Architecture", "Channel", "Release", "Ring", "Language", "Platform", "Product", "Branch", "JDK", "Title", "Edition", "Type" -ErrorAction "SilentlyContinue" | `
-                ConvertTo-Json | `
-                Out-File -FilePath $([System.IO.Path]::Combine($Path, "$_.json")) -NoNewline -Encoding "utf8" -Verbose
+                ConvertTo-Json | Out-File -FilePath $([System.IO.Path]::Combine($Path, "$App.json")) -NoNewline -Encoding "utf8" -Verbose
         }
         else {
             Write-Host -Object "Encountered an issue with: $_" -ForegroundColor "Cyan"
@@ -51,11 +52,12 @@ else {
     foreach ($file in (Get-ChildItem -Path $Path -Filter "*.json")) {
         if (($file.Length -eq 0) -or ((Get-Content -Path $file.FullName) -match "RateLimited")) {
             Write-Host -Object "Update: $($file.BaseName)." -ForegroundColor "Cyan"
+
             $Output = Get-EvergreenApp -Name $file.BaseName -ErrorAction "SilentlyContinue" -WarningAction "SilentlyContinue"
             if ($Null -ne $Output -or $Output.Version -ne "RateLimited") {
+
                 $Output | Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true }, "Architecture", "Channel", "Release", "Ring", "Language", "Platform", "Product", "Branch", "JDK", "Title", "Edition", "Type" -ErrorAction "SilentlyContinue" | `
-                    ConvertTo-Json | `
-                    Out-File -FilePath $file.FullName -NoNewline -Encoding "utf8" -Force -Verbose
+                    ConvertTo-Json | Out-File -FilePath $file.FullName -NoNewline -Encoding "utf8" -Force -Verbose
             }
             Remove-Variable -Name "Output" -ErrorAction "SilentlyContinue"
         }
