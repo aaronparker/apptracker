6d82329deea307e4b857c63c6978668ca6878210
Aaron Parker <aaron@stealthpuppy.com>
2022-05-18 18:44:34 +1000

    Update scripts

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 An application version tracker that uses [Evergreen](https://stealthpuppy.com/evergreen) to query for application version updates and store the result in JSON format.
 
-A workflow is run once very 24-hours to retrieve available updates and commit changes to this repository.
+A workflow is run every 4-hours to retrieve available updates and commit changes to this repository.
 
 [![update-json](https://github.com/aaronparker/apptracker/actions/workflows/update-json.yml/badge.svg?branch=main&event=schedule)](https://github.com/aaronparker/apptracker/actions/workflows/update-json.yml)
---
+++ scripts/New-Report.ps1
@@ -0,0 +1,28 @@
+$Path = "./docs/json/*.json"
+$OutFile = "./docs/apps.md"
+
+Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.208"
+If (Get-PSRepository -Name "PSGallery" | Where-Object { $_.InstallationPolicy -ne "Trusted" }) {
+    Write-Host "Trust repository: PSGallery." -ForegroundColor "Cyan"
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+}
+$Modules = @("MarkdownPS")
+ForEach ($Module in $Modules) {
+    If ([System.Version]((Find-Module -Name $Module).Version) -gt (Get-Module -Name $Module -ListAvailable).Version) {
+        Write-Host "Checking module $Module." -ForegroundColor "Cyan"
+        Install-Module -Name $Module -SkipPublisherCheck -Force
+        Import-Module -Name $Module -Force
+    }
+}
+
+# Update the list of supported apps in APPS.md
+$Markdown = New-MDHeader -Text "Application Versions" -Level 1
+$Markdown += "`n"
+foreach ($File in (Get-ChildItem -Path $Path)) {
+    $Markdown += New-MDHeader -Text "$($File.BaseName)" -Level 2
+    $Markdown += "`n"
+    $Table = Get-Content -Path $File.FullName | ConvertFrom-Json | New-MDTable
+    $Markdown += $Table
+    $Markdown += "`n"
+}
+($Markdown.TrimEnd("`n")) | Out-File -FilePath $OutFile -Force -Encoding "Utf8"
