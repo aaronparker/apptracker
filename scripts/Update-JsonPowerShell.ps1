7ba664f9e39563fcc044c9475e64539a866c7d57
Aaron Parker <aaron@stealthpuppy.com>
2024-04-10 08:19:30 +1000

    Split update JSON scripts

---
+++ scripts/Update-JsonPowerShell.ps1
@@ -0,0 +1,67 @@
+<#
+    Queries each application in Evergreen and exports the result to JSON
+#>
+[CmdletBinding(SupportsShouldProcess = $true)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "")]
+param(
+    [ValidateNotNullOrEmpty()]
+    [System.String] $Path
+)
+
+# Apps that should be skipped in this run
+$SkipApps = @("MozillaFirefox", "MozillaThunderbird")
+
+# Step through all apps and export result to JSON
+Import-Module -Name "Evergreen" -Force
+
+# Walk-through each JSON file and validate it, update contents if required
+foreach ($file in (Get-ChildItem -Path $Path -Filter "*.json")) {
+    if (($file.Length -eq 0) -or ((Get-Content -Path $file.FullName) -match "RateLimited")) {
+
+        Write-Host -Object "Update: $($file.BaseName)." -ForegroundColor "Cyan"
+        $params = @{
+            Name          = $file.BaseName
+            ErrorAction   = "SilentlyContinue"
+            WarningAction = "SilentlyContinue"
+            Verbose       = $true
+        }
+        $Output = Get-EvergreenApp @params
+
+        if ($null -eq $Output) {
+            Write-Host -Object "Encountered an issue with: $($file.BaseName)." -ForegroundColor "Cyan"
+        }
+        elseif ($Output[0].Version -eq "RateLimited") {
+            Write-Host -Object "Skipping. GitHub API rate limited: $($file.BaseName)." -ForegroundColor "Cyan"
+        }
+        else {
+            ConvertTo-Json -InputObject @($Output | Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true }, "Platform", "Type", "Architecture", "Channel", "Release", "Ring", "Language", "Product", "Branch", "JDK", "Title", "Edition" -ErrorAction "SilentlyContinue") | Out-File -FilePath $file.FullName -NoNewline -Encoding "utf8" -Verbose
+            Remove-Variable -Name "Output" -ErrorAction "SilentlyContinue"
+        }
+    }
+}
+
+# Find output that doesn't exist for an application in Evergreen
+foreach ($App in (Find-EvergreenApp | Where-Object { $_.Name -notin $SkipApps } | Sort-Object { Get-Random } | Select-Object -ExpandProperty "Name")) {
+    if (-not (Test-Path -Path $([System.IO.Path]::Combine($Path, "$App.json")) -ErrorAction "SilentlyContinue")) {
+
+        Write-Host -Object "Update: $App." -ForegroundColor "Cyan"
+        $params = @{
+            Name          = $App
+            ErrorAction   = "SilentlyContinue"
+            WarningAction = "SilentlyContinue"
+            Verbose       = $true
+        }
+        $Output = Get-EvergreenApp @params
+
+        if ($null -eq $Output) {
+            Write-Host -Object "Encountered an issue with: $App." -ForegroundColor "Cyan"
+        }
+        elseif ("RateLimited" -in $Output.Version) {
+            Write-Host -Object "Skipping. GitHub API rate limited: $App." -ForegroundColor "Cyan"
+        }
+        else {
+            ConvertTo-Json -InputObject @($Output | Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true }, "Platform", "Type", "Architecture", "Channel", "Release", "Ring", "Language", "Product", "Branch", "JDK", "Title", "Edition" -ErrorAction "SilentlyContinue") | Out-File -FilePath $([System.IO.Path]::Combine($Path, "$App.json")) -NoNewline -Encoding "utf8" -Verbose
+            Remove-Variable -Name "Output" -ErrorAction "SilentlyContinue"
+        }
+    }
+}
