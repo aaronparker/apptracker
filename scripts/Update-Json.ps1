4257019a9c686a9a4f55b4d8b06253aa21d960a4
Aaron Parker <aaron@stealthpuppy.com>
2024-04-10 08:42:18 +1000

    Update script
    Ensure Pwsh and PowerShell run the same actions

--- scripts/Update-Json.ps1
+++ scripts/Update-Json.ps1
@@ -14,6 +14,37 @@ $SkipApps = @("MozillaFirefox", "MozillaThunderbird")
 # Step through all apps and export result to JSON
 Import-Module -Name "Evergreen" -Force
 
+# Remove extra files
+$Files = Get-ChildItem -Path $Path -Filter "*.json" | Select-Object -ExpandProperty "Basename"
+$Apps = Find-EvergreenApp | Select-Object -ExpandProperty "Name"
+Compare-Object -ReferenceObject $Files -DifferenceObject $Apps | `
+    Select-Object -ExpandProperty "InputObject" | `
+    ForEach-Object { Remove-Item -Path $([System.IO.Path]::Combine($Path, "$($_).json")) -ErrorAction "SilentlyContinue" }
+
+# Walk-through each Evergreen app and export data to JSON files
+foreach ($App in (Find-EvergreenApp | Where-Object { $_.Name -notin $SkipApps } | Sort-Object { Get-Random } | Select-Object -ExpandProperty "Name")) {
+
+    try {
+        $Output = Get-EvergreenApp -Name $App -ErrorAction "SilentlyContinue" -WarningAction "SilentlyContinue"
+    }
+    catch {
+        Write-Host -Object "Encountered an issue with: $App." -ForegroundColor "Cyan"
+        Write-Host -Object $_.Exception.Message -ForegroundColor "Cyan"
+        $Output = $null
+    }
+
+    if ($null -eq $Output) {
+        Write-Host -Object "Output from app is null: $App." -ForegroundColor "Cyan"
+    }
+    elseif ("RateLimited" -in $Output.Version) {
+        Write-Host -Object "Skipping. GitHub API rate limited: $App." -ForegroundColor "Cyan"
+    }
+    else {
+        ConvertTo-Json @($Output | Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true }, "Platform", "Type", "Architecture", "Channel", "Release", "Ring", "Language", "Product", "Branch", "JDK", "Title", "Edition" -ErrorAction "SilentlyContinue") | Out-File -FilePath $([System.IO.Path]::Combine($Path, "$App.json")) -NoNewline -Encoding "utf8" -Verbose
+        Remove-Variable -Name "Output" -ErrorAction "SilentlyContinue"
+    }
+}
+
 # Walk-through each JSON file and validate it, update contents if required
 foreach ($file in (Get-ChildItem -Path $Path -Filter "*.json")) {
     if (($file.Length -eq 0) -or ((Get-Content -Path $file.FullName) -match "RateLimited")) {
--- scripts/Update-Others.ps1
+++ scripts/Update-Others.ps1
@@ -8,30 +8,6 @@ param(
     [System.String] $Path
 )
 
-#region Functions
-function Test-PSCore {
-    <#
-        .SYNOPSIS
-            Returns True if running on PowerShell Core.
-    #>
-    [CmdletBinding(SupportsShouldProcess = $false)]
-    [OutputType([Boolean])]
-    param (
-        [Parameter(Mandatory = $false, Position = 0)]
-        [ValidateNotNullOrEmpty()]
-        [System.String] $Version = '6.0.0'
-    )
-
-    # Check whether current PowerShell environment matches or is higher than $Version
-    if (($PSVersionTable.PSVersion -ge [Version]::Parse($Version)) -and ($PSVersionTable.PSEdition -eq "Core")) {
-        Write-Output -InputObject $true
-    }
-    else {
-        Write-Output -InputObject $false
-    }
-}
-#endregion
-
 # Step through all apps and export result to JSON
 Import-Module -Name "Evergreen" -Force
 
