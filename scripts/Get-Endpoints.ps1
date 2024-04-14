37d920d43886d8d06efdeb3374dcfddfc7432cf6
Aaron Parker <aaron@stealthpuppy.com>
2024-04-14 17:23:20 +1000

    Update Get-Endpoints.ps1

--- scripts/Get-Endpoints.ps1
+++ scripts/Get-Endpoints.ps1
@@ -5,14 +5,20 @@
 
 $Namespace = "037069e7da3e4944be2cbc97c92409a5"
 $UrlMatch = "http[s]?\:\/\/([^\/?#]+)(?:[\/?#]|$)"
+$EvergreenManifests = "/Users/aaron/projects/evergreen/Evergreen/Manifests"
+$AppTrackerJson = "/Users/aaron/projects/apptracker/json"
 
 # Get endpoint URLs from Evergreen manifests and post to the version endpoint
-$Endpoints = Get-ChildItem -Path "/Users/aaron/projects/evergreen/Evergreen/Manifests" -Recurse -Include "*.json" | ForEach-Object {
+$Endpoints = Get-ChildItem -Path $EvergreenManifests -Recurse -Include "*.json" | ForEach-Object {
     [PSCustomObject]@{
         Application = $_.BaseName
-        Endpoints   = ((((Select-String -Path $_.FullName -Pattern $UrlMatch).Matches.Value | `
-                        Select-Object -Unique | `
-                        Sort-Object) -replace "http://|https://", "").TrimEnd("/|#"))
+        Endpoints   = @(((((Select-String -Path $_.FullName -Pattern $UrlMatch).Matches.Value | `
+                            Select-Object -Unique | `
+                            Sort-Object) -replace "http://|https://", "").TrimEnd("/|#")))
+        Ports       = @(((((Select-String -Path $_.FullName -Pattern $UrlMatch).Matches.Value | Select-Object -Unique))) | ForEach-Object {
+                if ($_ -match "http://") { "80" }
+                if ($_ -match "https://") { "443" }
+            } | Select-Object -Unique | Sort-Object -Descending)
     }
 }
 $Endpoints | ConvertTo-Json | Out-File -FilePath "./Endpoints.json" -Encoding "Utf8" -NoNewline
@@ -20,12 +26,16 @@ wrangler kv:key put "endpoints-versions" --path="./Endpoints.json" --namespace-i
 Remove-Item -Path "./Endpoints.json"
 
 # Get endpoint URLs for downloads from the Evergreen AppTracker and port to the downloads endpoint
-$Endpoints = Get-ChildItem -Path "/Users/aaron/projects/apptracker/json" -Recurse -Include "*.json" | ForEach-Object {
+$Endpoints = Get-ChildItem -Path $AppTrackerJson -Recurse -Include "*.json" | ForEach-Object {
     [PSCustomObject]@{
         Application = $_.BaseName
-        Endpoints   = ((((Select-String -Path $_.FullName -Pattern $UrlMatch).Matches.Value | `
-                        Select-Object -Unique | `
-                        Sort-Object) -replace "http://|https://", "").TrimEnd("/|#"))
+        Endpoints   = @(((((Select-String -Path $_.FullName -Pattern $UrlMatch).Matches.Value | `
+                            Select-Object -Unique | `
+                            Sort-Object) -replace "http://|https://", "").TrimEnd("/|#")))
+        Ports       = @(((((Select-String -Path $_.FullName -Pattern $UrlMatch).Matches.Value | Select-Object -Unique))) | ForEach-Object {
+                if ($_ -match "http://") { "80" }
+                if ($_ -match "https://") { "443" }
+            } | Select-Object -Unique | Sort-Object -Descending)
     }
 }
 $Endpoints | ConvertTo-Json | Out-File -FilePath "./Endpoints.json" -Encoding "Utf8" -NoNewline
